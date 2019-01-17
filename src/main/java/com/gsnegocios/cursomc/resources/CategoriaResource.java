ade1c611176971e5cfa0d312894d98879d1e7a13
Flavio Cunha <flavio.cunha@gsnegocios.com.br>
2019-01-16 23:21:08 -0200

    Carga automatica

--- src/main/java/com/gsnegocios/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/gsnegocios/cursomc/resources/CategoriaResource.java
@@ -1,6 +1,4 @@
 package com.gsnegocios.cursomc.resources;
-import java.util.ArrayList;
-import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -8,7 +6,6 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
-
 import com.gsnegocios.cursomc.domain.Categoria;
 import com.gsnegocios.cursomc.services.CategoriaService;
 
